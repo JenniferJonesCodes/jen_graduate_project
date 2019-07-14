985628ac83a8d6e8d50248caacecff7e7eb40d3c
Jennifer Jones <jenniferalexisjones@gmail.com>
2019-07-14 17:46:59 -0400

    db connection to test

--- myApi/app.js
+++ myApi/app.js
@@ -1,6 +1,30 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.route('/patients')
+  .get(function(req, res, next) {
+    connection.query(
+      "SELECT * FROM `patients`",
+      function(error, results, fields) {
+        if (error) throw error;
+        res.json(results);
+      }
+    );
+  });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.get('/status', (req, res) => res.send('Working!'));
+
+// Port 8080 for Google App Engine
+app.set('port', process.env.PORT || 3000);
+app.listen(3000);
+
+//const express = require('express');
+//const app = express();
+
+//app.get('/', (req, res) => res.send('Hello World!'));
+
+//app.listen(3000, () => console.log('Example app listening on port 3000!'));
