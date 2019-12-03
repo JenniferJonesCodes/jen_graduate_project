a15f29d5693743160d49b427e87d24087229a63f
Jennifer Jones <jenniferalexisjones@gmail.com>
2019-12-02 20:20:04 -0500

    auth in each request

--- rest_client/src/lib/client.js
+++ rest_client/src/lib/client.js
@@ -4,15 +4,16 @@ const axios = require("axios");
 
 const instance = axios.create({
   baseURL: "https://demo.mybahmni.org//openmrs/ws/rest/v1/",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
+  headers: { Accept: "application/json", "Content-Type": "application/json" },
+  withCredentials: true
 });
 
 module.exports = instance;
 
 module.exports.setToken = function setToken(token) {
-  instance.defaults.headers.Cookie = token;
+  instance.defaults.headers.Authorization = token;
 };
 
 module.exports.clearToken = function clearToken() {
-  instance.defaults.headers.Cookie = "";
+  instance.defaults.headers.Authorization = "";
 };
