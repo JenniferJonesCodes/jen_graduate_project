ef3bc84052a2275f2de5fbbf877bb5e54da7359f
Jennifer Jones <jenniferalexisjones@gmail.com>
2019-12-02 20:20:30 -0500

    update test to include concept and encouter

--- rest_client/test/index.js
+++ rest_client/test/index.js
@@ -2,8 +2,22 @@ const session = require("./session");
 const user = require("./user");
 const patient = require("./patient");
 const visit = require("./visit");
+const encounter = require("./encounter");
+const concept = require("./concept");
+
+let conceptCache = {};
+let patientIdCache = null;
 
 session()
   .then(() => user())
   .then(() => patient())
-  .then(patientID => visit(patientID));
+  .then(patientID => {
+    patientIdCache = patientID;
+    return visit(patientID);
+  })
+  .then(() => concept())
+  .then(conceptResult => {
+    conceptCache = conceptResult;
+    return Promise.resolve();
+  })
+  .then(() => encounter({ concepts: conceptCache, patientID: patientIdCache }));
